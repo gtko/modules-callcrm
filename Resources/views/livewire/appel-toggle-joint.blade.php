c8dad77386031823ecbf247b7381b4db5e11ea32
gtko <gtux.prog@gmail.com>
2022-09-29 12:06:01 +0200

    git rebase

--- Resources/views/livewire/appel-toggle-joint.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="my-2">
-    <span class="btn-sm text-white w-48 bg-red-600 cursor-pointer rounded" wire:click="toogle(0)">Non-joint</span>
-    <span class="btn-sm text-white w-48 bg-green-600 cursor-pointer rounded" wire:click="toogle(1)">Joint</span>
-</div>
